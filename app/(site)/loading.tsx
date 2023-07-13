c75985090376701cf7cb475467b573ac06d68e39
ayan471 <pakhiraayan@gmail.com>
2023-07-13 17:39:24 +0530

    creating the stripe payment intregation

---
+++ app/(site)/loading.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import Box from "@/components/Box";
+
+const Loading = () => {
+  return (
+    <Box className="h-full flex items-center justify-center">Loading...</Box>
+  );
+};
+
+export default Loading;
